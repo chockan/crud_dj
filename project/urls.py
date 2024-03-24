2875296d1ae806646bbb8ec499cddf37aa3babc7
chockan <chockansamy@gmail.com>
2024-03-24 10:26:29 +0530

    gg

--- project/urls.py
+++ project/urls.py
@@ -25,11 +25,11 @@ from app1 import views
 urlpatterns = [  
     path('admin/', admin.site.urls), 
     # path('app1/', include('app1.urls')), 
-    path('emp', views.emp),  
-    path('', views.emp),  
-    path('show',views.show),  
-    path('edit/<int:id>', views.edit),  
-    path('update/<int:id>', views.update),  
-    path('delete/<int:id>', views.destroy),
+    path('emp', views.emp,name='emp'),  
+    path('', views.emp,name='emp'),  
+    path('show',views.show,name='show'),  
+    path('edit/<int:id>', views.edit,name='edit'),  
+    path('update/<int:id>', views.update,name='update'),  
+    path('delete/<int:id>', views.destroy,name='delete'),
 
 ]  
